357cbcbe1e5448b817d26145911e8da5bcaf6484
mykoakbar <yeikoakbar20@gmail.com>
2025-03-11 22:02:02 +0700

    number page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Hero from "./components/Product/hero";
 import { useState, useEffect } from "react";
-import { useRouter, useParams } from "next/navigation";
 
 interface Data {
   count: number;
@@ -19,6 +18,7 @@ export default function Home() {
 
   const [Data, setData] = useState<Data | null>(null);
   const [Page, setPage] = useState<number>(0);
+  const [PageNumber, setPageNumber] = useState<number>(1);
   const [LoadingPage, setLoadingPage] = useState<boolean>(false);
 
   useEffect(() => {
@@ -43,20 +43,27 @@ export default function Home() {
   }, [Page]);
 
   return (
-    <div className="flex flex-wrap gap-2 items-center justify-center h-full p-5 bg-[#1D1616]">
-      {Data?.results.map((item: results, index: number) => (
-        <div className="flex gap-1" key={index}>
-          <Hero
-            nama={item.name}
-          />
-        </div>
-      ))}
+    <>
+      {/* SCREEN */}
+      <h1 className="absolute font-bold text-4xl ml-3 mt-3 text-white">{PageNumber}</h1>
+      <div className="flex flex-wrap gap-2 items-center justify-center min-h-screen p-5 bg-[#1D1616]">
+        {Data?.results.map((item: results, index: number) => (
+          <div className="flex gap-1" key={index}>
+            <Hero
+              nama={item.name}
+            />
+          </div>
+        ))}
+      </div>
       {/* FOOTER NEXT PREV */}
-      <div className={`flex justify-between items-center w-full rounded-4xl p-5 my-5 text-white ${LoadingPage ? 'animate-pulse bg-red-900' : 'bg-red-400'}`}>
+      <div className={`flex bottom-0 top-0 left-0 right-0 sticky justify-between items-center w-full rounded-4xl p-5 text-white ${LoadingPage ? 'animate-pulse bg-red-900' : 'bg-red-400'}`}>
         <div className="pl-4">
           {Data?.previous &&
             <button
-              onClick={() => setPage((prev) => prev - 18)}
+              onClick={() => {
+                setPage((prev) => prev - 18);
+                setPageNumber((prev) => prev - 1);
+              }}
               type="button"
               disabled={LoadingPage}
               className="uppercase font-bold border rounded-xl border-white px-5 py-2 hover:bg-white hover:text-red-400 cursor-pointer"
@@ -68,7 +75,10 @@ export default function Home() {
         <div className="pr-4">
           {Data?.next &&
             <button
-              onClick={() => setPage((prev) => prev + 18)}
+              onClick={() => {{
+                setPage((prev) => prev + 18);
+                setPageNumber((prev) => prev + 1);
+              }}}
               type="button"
               disabled={LoadingPage}
               className="uppercase font-bold border rounded-xl border-white px-5 py-2 hover:bg-white hover:text-red-400 cursor-pointer"
@@ -78,6 +88,6 @@ export default function Home() {
           }
         </div>
       </div>
-    </div>
+    </>
   );
 }
